d610b394236551519bbf324a0a7bd0445a610497
Victor Irekponor <marquisvictor93@gmail.com>
2020-10-16 12:58:57 +0100

    :constructioin:

--- src/components/layouts/Content.js
+++ src/components/layouts/Content.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import { Header } from './Header';
 import { Sidebar } from "./Sidebar";
+import { Tasks } from '../Tasks'
 
 export const Content = () => {
   return (
     <section>
       <Sidebar />
+      <Tasks />
     </section>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app';
-import firebase from 'firestore';
+import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyBiYCsMcXRLuFBuFOE3pJRWNtLZzuWULKc",
+    authDomain: "todoist-fb821.firebaseapp.com",
+    databaseURL: "https://todoist-fb821.firebaseio.com",
+    projectId: "todoist-fb821",
+    storageBucket: "todoist-fb821.appspot.com",
+    messagingSenderId: "326145727699",
+    appId: "1:326145727699:web:fccb068f435667f7a04826",
+    measurementId: "G-BM5VQJQN2M"
 })
 
 
